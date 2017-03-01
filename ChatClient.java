ed1395023088e4cdd6216fba0fbb48e0460c9810
hieutrung1801 <hieutrung180196@gmail.com>
2017-03-01 22:10:41 +0700

    Update
    Auto chạy được dữ liệu từ TPHCM

--- ChatClient.java
+++ ChatClient.java
@@ -1,11 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.DataInputStream;
-import java.io.PrintStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -40,39 +36,42 @@ public class ChatClient implements Runnable {
     /*
      * Open a socket on a given host and port. Open input and output streams.
      */
-
-        try {
-
-            clientSocket = new Socket(host, portNumber);
-            inputLine = new BufferedReader(new InputStreamReader(System.in));
-            os = new PrintStream(clientSocket.getOutputStream());
-            is = new DataInputStream(clientSocket.getInputStream());
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + host);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to the host "
-                    + host);
-        }
-
-        if (clientSocket != null && os != null && is != null) {
             try {
 
+
+                clientSocket = new Socket(host, portNumber);
+                inputLine = new BufferedReader(new InputStreamReader(System.in));
+                os = new PrintStream(clientSocket.getOutputStream());
+                is = new DataInputStream(clientSocket.getInputStream());
+            } catch (UnknownHostException e) {
+                System.err.println("Don't know about host " + host);
+            } catch (IOException e) {
+                System.err.println("Couldn't get I/O for the connection to the host "
+                        + host);
+            }
+
+
+            if (clientSocket != null && os != null && is != null) {
+                try {
+
         /* Create a thread to read from the server. */
-                new Thread(new ChatClient()).start();
-                while (!closed) {
-                    os.println(inputLine.readLine().trim());
-                }
+                    new Thread(new ChatClient()).start();
+                    while (!closed) {
+                        os.println(inputLine.readLine().trim());
+                    }
+
         /*
          * Close the output stream, close the input stream, close the socket.
          */
-                os.close();
-                is.close();
-                clientSocket.close();
-            } catch (IOException e) {
-                System.err.println("IOException:  " + e);
+                    os.close();
+                    is.close();
+                    clientSocket.close();
+                } catch (IOException e) {
+                    System.err.println("IOException:  " + e);
+                }
             }
         }
-    }
+
 
 
     public void run() {
